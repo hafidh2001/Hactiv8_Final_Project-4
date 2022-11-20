ab227969c18dec28dc1d68d1a0f0dba3234deea7
hafidh2001 <hafidhfauzan2@gmail.com>
2022-11-20 11:54:51 +0700

    fixing migration

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ dotenv.config();
 const port = process.env.PORT;
 const node_env = process.env.NODE_ENV;
 const database_url = process.env.DATABASE_URL;
+const database_url_test = process.env.DATABASE_URL_TEST;
 const jwt_secret = process.env.JWT_SECRET;
 
-export { port, database_url, node_env, jwt_secret };
+export { port, database_url, database_url_test, node_env, jwt_secret };

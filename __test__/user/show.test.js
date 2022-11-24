8e4b7d3c66cb36f578a3e9737dd213dabd6aceaf
hafidh2001 <hafidhfauzan2@gmail.com>
2022-11-24 17:05:01 +0700

    authentication

--- __test__/user/show.test.js
+++ __test__/user/show.test.js
@@ -3,22 +3,12 @@ import app from "../../app.js";
 import jwt from "jsonwebtoken";
 import { jwt_secret } from "../../config.js";
 
-const user = {
-    full_name: "programmer",
-    email: "programmer@gmail.com",
-    username: "programmer",
-    password: "12345",
-    profile_image_url: "https://photo.jpg",
-    age: 21,
-    phone_number: "111111111111",
-    createdAt: new Date(),
-    updatedAt: new Date()
-};
-
-const userToken = jwt.sign({ id: 1, email: user.email }, jwt_secret, {
+const userToken = jwt.sign({ id: 1, email: "programmer@gmail.com" }, jwt_secret, {
     expiresIn: "24h",
   })
-console.log(userToken, typeof userToken)
+const userNotExistsToken = jwt.sign({ id: 100, email: "designer@gmail.com" }, jwt_secret, {
+    expiresIn: "24h",
+  })
 
 describe("GET /users/", () => {
   // SUCCESS
@@ -29,12 +19,20 @@ describe("GET /users/", () => {
       expect.stringContaining("json")
     );
   });
-  
-//   test("HTTP status code 200 (show success)", async () => {
-//     const res = await request(app).get("/users/");
-//     expect(res.status).toEqual(200);
-//     expect(res.headers["content-type"]).toEqual(
-//       expect.stringContaining("json")
-//     );
-//   });
+
+  // ERROR
+  test("HTTP status code 401 (credentials not found)", async () => {
+    const res = await request(app).get("/users/");
+    expect(res.status).toBe(401);
+    expect(res.body).toEqual({
+      status: "error",
+      message: "failed to access, credentials not found",
+    });
+  });
+
+  test("HTTP status code 401 (authorization failed)", async () => {
+    const res = await request(app).get("/users/").set('Authorization', `Bearer ${userNotExistsToken}`);
+    expect(res.status).toBe(401);
+    expect(res.body).toEqual({ status: "error", message: "authorization failed" });
+  });
 });
